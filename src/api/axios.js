f6363090ef7e3ee3b3f1141517b43a2795c2a094
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2024-05-03 10:57:32 +0900

    .env 생성하여 API KEY 은닉하기

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL : "https://api.themoviedb.org/3",
     params:{
-        api_key : "0a1e1ac69550c51c58f6d7efbcaa7d3b",
+        api_key : process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR"
     },
 });
