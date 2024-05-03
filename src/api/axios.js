5d84a571d50f04f590588ecd8a37894f351fa2ed
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2024-05-03 13:49:14 +0900

    commit

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-const instance = axios.create({
-    baseURL : "https://api.themoviedb.org/3",
-    params:{
-        api_key : process.env.REACT_APP_MOVIE_DB_API_KEY,
-        language: "ko-KR"
-    },
-});
-
-export default instance
-
-// https://api.themoviedb.org/3/search/multi?include_adult=false&query=spider&api_key=0a1e1ac69550c51c58f6d7efbcaa7d3b&language=ko-KR
+ const  instance = 
+    axios.create({
+        baseURL: "https://api.themoviedb.org/3",
+        params: {
+            api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
+            language: "ko-KR"
+        }
+    });
+    export default instance;
